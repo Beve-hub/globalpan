1480d91605fb4de1ded7865b509cdff094541417
Believe <117173080+Beve-hub@users.noreply.github.com>
2024-09-17 21:30:41 +0100

    update

--- src/pages/auth/Action.tsx
+++ src/pages/auth/Action.tsx
@@ -1,28 +1,25 @@
-import React, { useEffect, useState} from "react";
-import { useLocation,useNavigate } from "react-router-dom";
-import {  applyActionCode, checkActionCode,confirmPasswordReset} from "firebase/auth";
-import { auth } from "@/firebase";
-import Loader from "@/utils/reusable/Loader";
+import React, { useEffect, useState } from 'react';
+import { applyActionCode, checkActionCode, confirmPasswordReset } from 'firebase/auth';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { notifications } from '@mantine/notifications';
-
-
+import { auth } from '@/firebase';
+import Loader from '@/utils/reusable/Loader';
 
 interface Errors {
-    password?: string;
-    confirm?: string;
+  password?: string;
+  confirm?: string;
 }
 
-
 const Action = () => {
-    const location = useLocation();
-    const navigate = useNavigate();
-    const queryParams = new URLSearchParams(location.search);
-    const mode = queryParams.get("mode");
-    const oobCode = queryParams.get("oobCode"); 
-    const [password, setPassword] = useState('');
-  const [confirm, setConfirm] = useState('');   
-    const [loading, setLoading] = useState(true);
-    const [errors, setErrors] = useState<Errors>({});
+  const location = useLocation();
+  const navigate = useNavigate();
+  const queryParams = new URLSearchParams(location.search);
+  const mode = queryParams.get('mode');
+  const oobCode = queryParams.get('oobCode');
+  const [password, setPassword] = useState('');
+  const [confirm, setConfirm] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [errors, setErrors] = useState<Errors>({});
 
   useEffect(() => {
     // Simulate some asynchronous operation
@@ -30,56 +27,49 @@ const Action = () => {
       setLoading(false);
     }, 3000);
   }, []);
-   
+
   useEffect(() => {
     if (!mode || !oobCode) {
-        notifications.show({
-            title: `Error Alert `,
-            message: `Invalid request.`,
-            color: 'red',
-            position:'top-right',
-        });
-        return;
+      notifications.show({
+        title: `Error Alert `,
+        message: `Invalid request.`,
+        color: 'red',
+        position: 'top-right',
+      });
+      return;
     }
 
-   
     const handleAction = async () => {
-        
-        try {
-            if (mode === "resetPassword") {
-
-                // Verify the password reset code is valid
-                await checkActionCode(auth, oobCode);
-                       
-
-            } else if (mode === "verifyEmail") {
-
-                // Apply the email verification code
-                await applyActionCode(auth, oobCode);
-                navigate('/validId');
-                
-            } else {
-                notifications.show({
-                    title: `Error Alert `,
-                    message: `Invalid mode.`,
-                    color: 'red',
-                    position:'top-right',
-                });               
-            }
-        } catch (error) {
-            notifications.show({
-                title: `Error Alert `,
-                message: `Invalid request.`,
-                color: 'red',
-                position:'top-right',
-            });           
+      try {
+        if (mode === 'resetPassword') {
+          // Verify the password reset code is valid
+          await checkActionCode(auth, oobCode);
+        } else if (mode === 'verifyEmail') {
+          // Apply the email verification code
+          await applyActionCode(auth, oobCode);
+          navigate('/profileDetails');
+        } else {
+          notifications.show({
+            title: `Error Alert `,
+            message: `Invalid mode.`,
+            color: 'red',
+            position: 'top-right',
+          });
         }
+      } catch (error) {
+        notifications.show({
+          title: `Error Alert `,
+          message: `Invalid request.`,
+          color: 'red',
+          position: 'top-right',
+        });
+      }
     };
 
     handleAction();
-}, [mode, oobCode]);
+  }, [mode, oobCode]);
 
-const validate = (): boolean => {
+  const validate = (): boolean => {
     const errors: Errors = {};
     let isValid = true;
 
@@ -99,8 +89,7 @@ const validate = (): boolean => {
     return isValid;
   };
 
-
-const handleSubmit = async (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     if (!validate()) {
       return;
@@ -112,106 +101,109 @@ const handleSubmit = async (e: React.FormEvent) => {
       if (oobCode) {
         await confirmPasswordReset(auth, oobCode, password);
         notifications.show({
-            title: ` Successful `,
-            message: `Success! Your password has been changed successfully.`,
-            color: '#299165',
-            position:'top-right',
-        });        
+          title: ` Successful `,
+          message: `Success! Your password has been changed successfully.`,
+          color: '#299165',
+          position: 'top-right',
+        });
         navigate('/login');
       } else {
         notifications.show({
-            title: `Error Alert `,
-            message: `Invalid or missing oobCode.`,
-            color: 'red',
-            position:'top-right',
-        });          
+          title: `Error Alert `,
+          message: `Invalid or missing oobCode.`,
+          color: 'red',
+          position: 'top-right',
+        });
       }
     } catch (error) {
-      console.error("Error resetting password:", error);
+      console.error('Error resetting password:', error);
       notifications.show({
         title: `Error Alert `,
         message: `An error occurred while resetting the password. Please try again.`,
         color: 'red',
-        position:'top-right',
-    });     
+        position: 'top-right',
+      });
     } finally {
       setLoading(false);
     }
   };
-    return (
-        <div className="w-screen h-screen  bg-[--text-extra] grid justify-center items-center">                
-        {mode === "resetPassword" && (
-          
+  return (
+    <div className="w-screen h-screen  bg-[--text-extra] grid justify-center items-center">
+      {mode === 'resetPassword' && (
         <div className="h-screen  bg-[--text-extra] flex justify-center items-center">
-           {loading ? <Loader/> : (
-              <div className='grid mb-10'>        
+          {loading ? (
+            <Loader />
+          ) : (
+            <div className="grid mb-10">
+              <div className="w-screen grid items-center justify-center bg-gray-50 sm:px-6 lg:px-8">
+                <div className="w-full bg-[--text-extra]">
+                  <div>
+                    <h2 className=" text-start text-3xl font-extrabold text-gray-900">
+                      Password Recovery
+                    </h2>
+                    <p className="max-w-[16rem] py-2">
+                      Fill up the form to help you recover your account.
+                    </p>
+                  </div>
+                  <form className="space-y-6" onSubmit={handleSubmit}>
+                    <input type="hidden" name="remember" defaultValue="true" />
+                    <div className="grid gap-4">
+                      <div>
+                        <label htmlFor="password">New Password *</label>
+                        <input
+                          id="password"
+                          name="password"
+                          type="password"
+                          className="border-l-4 border-l-[--bg-color] block w-[20rem] px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                          placeholder="New Password"
+                          required
+                          value={password}
+                          onChange={(e) => setPassword(e.target.value)}
+                        />
+                        {errors.password && (
+                          <span className="text-[#f30000] text-sm">{errors.password}</span>
+                        )}
+                      </div>
 
-  <div className="w-screen grid items-center justify-center bg-gray-50 sm:px-6 lg:px-8">
-    <div className="w-full bg-[--text-extra]">
-      <div>
-        <h2 className=" text-start text-3xl font-extrabold text-gray-900">Password Recovery</h2>
-        <p className='max-w-[16rem] py-2'>Fill up the form to help you recover your account.</p>
-      </div>
-      <form className="space-y-6" onSubmit={handleSubmit}>
-        <input type="hidden" name="remember" defaultValue="true" />
-        <div className="grid gap-4">
-          <div>
-            <label htmlFor="password">New Password *</label>
-            <input
-              id="password"
-              name="password"
-              type="password"
-              className="border-l-4 border-l-[--bg-color] block w-[20rem] px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-              placeholder="New Password"
-              required
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            {errors.password && <span className='text-[#f30000] text-sm'>{errors.password}</span>}
-          </div>
+                      <div>
+                        <label htmlFor="confirm">Confirm New assword *</label>
+                        <input
+                          id="confirm"
+                          name="confirm"
+                          type="password"
+                          className="border-l-4 border-l-[--bg-color] block w-[20rem] px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                          required
+                          placeholder="Confirm Password"
+                          value={confirm}
+                          onChange={(e) => setConfirm(e.target.value)}
+                        />
+                        {errors.confirm && (
+                          <span className="text-[#f30000] text-sm">{errors.confirm}</span>
+                        )}
+                      </div>
+                    </div>
 
-          <div>
-            <label htmlFor="confirm">Confirm New assword *</label>
-            <input
-              id="confirm"
-              name="confirm"
-              type="password"
-              className="border-l-4 border-l-[--bg-color] block w-[20rem] px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-              required
-              placeholder="Confirm Password"
-              value={confirm}
-              onChange={(e) => setConfirm(e.target.value)} 
-            />
-            {errors.confirm && <span className='text-[#f30000] text-sm'>{errors.confirm}</span>}
-          </div>
-        </div>
-
-        <div>
-          <button
-            type="submit"
-            className="w-full text-[--text-extra] flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium bg-[--button-color]">
-            Submit
-          </button>
-        </div>
-      </form>
-    </div>
-  </div>
+                    <div>
+                      <button
+                        type="submit"
+                        className="w-full text-[--text-extra] flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium bg-[--button-color]"
+                      >
+                        Submit
+                      </button>
+                    </div>
+                  </form>
+                </div>
               </div>
-            ) }
+            </div>
+          )}
         </div>
-         )}
-       
-    
-         {mode === "verifyEmail" && (
-        <div className="flex justify-center items-center">
-        {loading ? <Loader/> : '' }
-         </div>
-         )}
-     
-   
-    
-    </div>
-    )
-}
+      )}
 
-export default Action
+      {mode === 'verifyEmail' && (
+        <div className="flex justify-center items-center">{loading ? <Loader /> : ''}</div>
+      )}
+    </div>
+  );
+};
+
+export default Action;
