5c5104aecf4ef14536fc1e12d14926bb0ceaa2a3
Believe <117173080+Beve-hub@users.noreply.github.com>
2024-09-23 07:38:06 +0100

    update

--- src/utils/data/TeamData.tsx
+++ src/utils/data/TeamData.tsx
@@ -23,7 +23,7 @@ interface SidebarItem {
 export const InvestBar: SidebarItem[] = [
     {
         name: 'Cory J. Robinson, CFA, FRM',
-        description: 'Mr. Robinson has been the President and Chief Investment Officer of Pan Global Trading Investment Management since 2003. He began his investment career in 1981, working with other PGTIM principals in the Trust Investments department of First National Bank and Trust Company which later became First Investment Management Corporation (FIMCO), a subsidiary of First Interstate Bank of Oklahoma City. Other work experience includes being a member of the Executive Committee Planning and Advisory Staff in 1981 and Staff Auditor in 1980 for First National Bank and Trust Company of Oklahoma City. He received his Bachelor of Science Degree in Business from the University of Colorado with an emphasis in international business and his MBA from Oklahoma City University. He is a CFA® charterholder, past adjunct Professor for Oklahoma City University and past President of the Oklahoma Society of Financial Analysts. Currently, he serves on the Board of Directors for the Oklahoma Medical Research Foundation, Oklahoma City University, and is a Director for The Economic Club of Oklahoma.  He is also a member of Rotary Club 29 of Oklahoma City, The Fortune Club, and an alumnus of Leadership Oklahoma City.',
+        description: 'Mr. Robinson has been the President and Chief Investment Officer of Nexcel Global Trading Investment Management since 2003. He began his investment career in 1981, working with other NGTIM principals in the Trust Investments department of First National Bank and Trust Company which later became First Investment Management Corporation (FIMCO), a subsidiary of First Interstate Bank of Oklahoma City. Other work experience includes being a member of the Executive Committee Planning and Advisory Staff in 1981 and Staff Auditor in 1980 for First National Bank and Trust Company of Oklahoma City. He received his Bachelor of Science Degree in Business from the University of Colorado with an emphasis in international business and his MBA from Oklahoma City University. He is a CFA® charterholder, past adjunct Professor for Oklahoma City University and past President of the Oklahoma Society of Financial Analysts. Currently, he serves on the Board of Directors for the Oklahoma Medical Research Foundation, Oklahoma City University, and is a Director for The Economic Club of Oklahoma.  He is also a member of Rotary Club 29 of Oklahoma City, The Fortune Club, and an alumnus of Leadership Oklahoma City.',
         image: IMG1,
         title: 'President & Chief Investment Officer',
         contact1: '(405) 236-2111',
@@ -32,7 +32,7 @@ export const InvestBar: SidebarItem[] = [
     },
     {
         name: 'Jeremy H. Wilcox, CFA, CFP®',
-        description: 'Mr. Wilcox joined Pan Global Trading Investment Management in October 1994 as Trader and Investment Officer and became Vice President and Portfolio Manager in 1997. Prior to TJIM, Mr. Haws worked as an Internal Auditor for Union Pacific Corporation in Omaha, Nebraska. As staff auditor, his responsibilities included comprehensive audits of both the financial and operational aspects of all corporate subsidiaries. Mr. Haws received his BBA in Finance from the University of Oklahoma and his MBA in Finance from The Wharton School, University of Pennsylvania. He is a CFA® charter holder, past adjunct Professor at the University of Oklahoma and past President of the Oklahoma Society of Financial Analysts. Mr. Haws serves on the Board of Directors for Bridges, a non-profit organization dedicated to empowering homeless students to succeed and is a member of the Oklahoma Christian Student Investment Fund Advisory Board.',
+        description: 'Mr. Wilcox joined Nexcel Global Trading Investment Management in October 1994 as Trader and Investment Officer and became Vice President and Portfolio Manager in 1997. Prior to NGTIM, Mr. Haws worked as an Internal Auditor for Union Pacific Corporation in Omaha, Nebraska. As staff auditor, his responsibilities included comprehensive audits of both the financial and operational aspects of all corporate subsidiaries. Mr. Haws received his BBA in Finance from the University of Oklahoma and his MBA in Finance from The Wharton School, University of Pennsylvania. He is a CFA® charter holder, past adjunct Professor at the University of Oklahoma and past President of the Oklahoma Society of Financial Analysts. Mr. Haws serves on the Board of Directors for Bridges, a non-profit organization dedicated to empowering homeless students to succeed and is a member of the Oklahoma Christian Student Investment Fund Advisory Board.',
         image: IMG2,
         title: 'Vice President & Portfolio Manager',
         contact1: '(405) 236-2111 x 109',
@@ -41,7 +41,7 @@ export const InvestBar: SidebarItem[] = [
     },
     {
         name: 'Nicholas J. Pointer, CFA',
-        description: 'Prior to joining Pan Global Trading Investment Management in September 2011, Mr. Pointer worked as an Investment Analyst for Oklahoma Teachers Retirement System in Oklahoma City, OK. His responsibilities included portfolio allocation, and performance analysis and reporting. From 2008 to 2009, Mr. Pointer was a Financial Analyst for American Airlines in Dallas, TX. Mr. Pointer received his BA in Political Science from the University of Oklahoma and his MBA in Corporate Finance/Investment Management from the University of Oklahoma. He is a CFA® charterholder, has served as the Industry Mentor to students at the University of Oklahoma in the CFA Research Challenge, serves as the Red River Kidney Care Entity Independent Consumer Advocate and is a past adjunct Professor at Oklahoma City University.',
+        description: 'Prior to joining Nexcel Global Trading Investment Management in September 2011, Mr. Pointer worked as an Investment Analyst for Oklahoma Teachers Retirement System in Oklahoma City, OK. His responsibilities included portfolio allocation, and performance analysis and reporting. From 2008 to 2009, Mr. Pointer was a Financial Analyst for American Airlines in Dallas, TX. Mr. Pointer received his BA in Political Science from the University of Oklahoma and his MBA in Corporate Finance/Investment Management from the University of Oklahoma. He is a CFA® charterholder, has served as the Industry Mentor to students at the University of Oklahoma in the CFA Research Challenge, serves as the Red River Kidney Care Entity Independent Consumer Advocate and is a past adjunct Professor at Oklahoma City University.',
         image: IMG3,
         title: 'Vice President, Portfolio Manager and Trader',
         contact1: '(405) 236-2111 X 111',
@@ -50,7 +50,7 @@ export const InvestBar: SidebarItem[] = [
     },    
     {
         name: 'Ryoma. Abe, CFA ',
-        description: 'Prior to joining Pan Global Trading Investment Management in September 2015, Mr. Abe graduated from Oklahoma State University with a Bachelor of Science in Business Administration with a major in Business Economics and Quantitative Studies and a minor in Finance. He is a CFA® charterholder and a CERTIFIED FINANCIAL PLANNER™ professional. He serves on the Church Council at Mayflower Congregational United Church of Christ and is also a member of Midtown Rotary Club of Oklahoma City.',
+        description: 'Prior to joining Nexcel Global Trading Investment Management in September 2015, Mr. Abe graduated from Oklahoma State University with a Bachelor of Science in Business Administration with a major in Business Economics and Quantitative Studies and a minor in Finance. He is a CFA® charterholder and a CERTIFIED FINANCIAL PLANNER™ professional. He serves on the Church Council at Mayflower Congregational United Church of Christ and is also a member of Midtown Rotary Club of Oklahoma City.',
         image: IMG5,
         title: 'President & Chief Investment Officer',
         contact1: '(405) 236-2111 X 112',
@@ -59,7 +59,7 @@ export const InvestBar: SidebarItem[] = [
     },
     {
         name: 'Kristy West',
-        description: 'Prior to joining Pan Global Trading Investment Management in June 2000, Mrs. West graduated from Oklahoma State University with a Bachelor of Science in Business Administration with a major in Finance and minors in Accounting and Management Information Systems. He is a CFA® charterholder and has received the FRM (Financial Risk Manager) designation from the Global Association of Risk Professionals. He is an officer of the CFA Society of Oklahoma and has served as an industry mentor for the University of Central Oklahoma CFA Institute Research Challenge.',
+        description: 'Prior to joining Nexcel Global Trading Investment Management in June 2000, Mrs. West graduated from Oklahoma State University with a Bachelor of Science in Business Administration with a major in Finance and minors in Accounting and Management Information Systems. He is a CFA® charterholder and has received the FRM (Financial Risk Manager) designation from the Global Association of Risk Professionals. He is an officer of the CFA Society of Oklahoma and has served as an industry mentor for the University of Central Oklahoma CFA Institute Research Challenge.',
         image: IMG4,
         title: 'Trader and Portfolio Manager',
         contact1: '(405) 236-2111 X 110',
@@ -68,7 +68,7 @@ export const InvestBar: SidebarItem[] = [
     },
     {
         name: 'Sophie Anderson',
-        description: 'Ms. Anderson joined Pan Global Trading Investment Management in April of 2019, having also worked for PGT in the capacity of Trader and Compliance Administrator from 1999 to 2004. Between these PGTIM employment time frames, Ms. Anderson worked at her family owned firm in an internal accounting capacity (Tripp Davis and Associates, a golf architectural firm). From 1996 to 1999, Ms. Davis was an Internal Wholesaler for Mid-America Partners, a third-party annuity marketing company. From 1992 to 1995, she worked in the Trust Department of Bank of Oklahoma. Ms. Anderson earned her Bachelor of Science degree in Finance from Oklahoma State University. Ms. Anderson is a non-voting member of the PGTIM Investment Committee.',
+        description: 'Ms. Anderson joined Nexcel Global Trading Investment Management in April of 2019, having also worked for PGT in the capacity of Trader and Compliance Administrator from 1999 to 2004. Between these NGTIM employment time frames, Ms. Anderson worked at her family owned firm in an internal accounting capacity (Tripp Davis and Associates, a golf architectural firm). From 1996 to 1999, Ms. Davis was an Internal Wholesaler for Mid-America Partners, a third-party annuity marketing company. From 1992 to 1995, she worked in the Trust Department of Bank of Oklahoma. Ms. Anderson earned her Bachelor of Science degree in Finance from Oklahoma State University. Ms. Anderson is a non-voting member of the PGTIM Investment Committee.',
         image: IMG6,
         title: 'Trader and Portfolio Manager',
         contact1: '(405) 236-2111 X 114',
@@ -83,7 +83,7 @@ export const AdminBar: SidebarItem[] = [
     
     {
         name: 'Richard H. Parry, CFA',
-        description: 'Since joining Pan Global Trading Investment Management at its inception, Mr. Parry has organized and is responsible for the accounting and operational areas of the company. Prior to joining Pan Global Trading Investment Management, she worked at the First National Bank and Trust Company of Oklahoma City in the Trust Investments Department where she coordinated and balanced investment reports. Ms. Calfy received her BA degree in 1981 from Northwest Nazarene College in Nampa, Idaho.',
+        description: 'Since joining Nexcel Global Trading Investment Management at its inception, Mr. Parry has organized and is responsible for the accounting and operational areas of the company. Prior to joining Nexcel Global Trading Investment Management, she worked at the First National Bank and Trust Company of Oklahoma City in the Trust Investments Department where she coordinated and balanced investment reports. Ms. Calfy received her BA degree in 1981 from Northwest Nazarene College in Nampa, Idaho.',
         image: IMG7,
         title: 'Senior Vice President & Chief Compliance Officer',
         contact1: '(405) 236-2111 X 108',
@@ -92,7 +92,7 @@ export const AdminBar: SidebarItem[] = [
     },
     {
         name: 'Cliff Urban',
-        description: 'Prior to joining Pan Global Trading Investment Management in December 1997, Mr. Urban graduated from the University of Central Oklahoma with a Bachelor of Arts degree in Psychology with a minor in Sociology. Ms. Bish is responsible for subadvisor/wrap programs, proxy information and requests, due diligence information, and questionnaire requests and database input.',
+        description: 'Prior to joining Nexcel Global Trading Investment Management in December 1997, Mr. Urban graduated from the University of Central Oklahoma with a Bachelor of Arts degree in Psychology with a minor in Sociology. Ms. Bish is responsible for subadvisor/wrap programs, proxy information and requests, due diligence information, and questionnaire requests and database input.',
         image: IMG8,
         title: 'Client/Marketing Liaison & Proxy Administrator',
         contact1: '(405) 236-2111 X 106',
@@ -101,7 +101,7 @@ export const AdminBar: SidebarItem[] = [
     },
     {
         name: 'Jenny Davis',
-        description: 'Mrs. Davis joined Pan Global Trading Investment Management, LLC in July 1990 and is responsible for all invoicing and reporting to Pan Global Trading Investment Management advisory clients. Her duties also include daily trade reconciliation and client relations.',
+        description: 'Mrs. Davis joined Nexcel Global Trading Investment Management, LLC in July 1990 and is responsible for all invoicing and reporting to Nexcel Global Trading Investment Management advisory clients. Her duties also include daily trade reconciliation and client relations.',
         image: IMG9,
         title: 'Client Reporting, Invoicing & Office Administrator',
         contact1: '(405) 236-2111 X 113',
@@ -110,7 +110,7 @@ export const AdminBar: SidebarItem[] = [
     },
     {
         name: 'Cindy Woods',
-        description: 'Mrs. Woods joined Pan Global Trading Investment Management, LLC in April 2017. Her responsibilities include daily account reconciliation, security litigations and other duties in the operations department.',
+        description: 'Mrs. Woods joined Nexcel Global Trading Investment Management, LLC in April 2017. Her responsibilities include daily account reconciliation, security litigations and other duties in the operations department.',
         image: IMG10,
         title: 'Operations Officer',
         contact1: '(405) 236-2111 X 100',
@@ -119,7 +119,7 @@ export const AdminBar: SidebarItem[] = [
     },
     {
         name: 'Angel Bish',
-        description: 'Mrs. Bish joined Pan Global Trading Investment Management, LLC in January 2000.  Her responsibilities include daily account reconciliation, security litigations and other duties in the operations department.',
+        description: 'Mrs. Bish joined Nexcel Global Trading Investment Management, LLC in January 2000.  Her responsibilities include daily account reconciliation, security litigations and other duties in the operations department.',
         image: IMG11,
         title: 'Operations Assistant',
         contact1: '(405) 236-2111 X 105',
@@ -129,7 +129,7 @@ export const AdminBar: SidebarItem[] = [
   
     {
         name: 'Lucy Lynskey',
-        description: 'Ms.Lynskey joined Pan Global Trading Investment Management, in January 2000. Her responsibilities include new advisor accounts, client relations and compliance administration. Prior to joining Pan Global Trading Investment Management, Ms. Lynskey worked at Exchange National Bank in Moore, OK as Assistant Cashier & Manager of the Bookkeeping Department.',
+        description: 'Ms.Lynskey joined Nexcel Global Trading Investment Management, in January 2000. Her responsibilities include new advisor accounts, client relations and compliance administration. Prior to joining Pan Global Trading Investment Management, Ms. Lynskey worked at Exchange National Bank in Moore, OK as Assistant Cashier & Manager of the Bookkeeping Department.',
         image: IMG12,
         title: 'Account Services & Compliance Administrator',
         contact1: '(405) 236-2111 X 105',
